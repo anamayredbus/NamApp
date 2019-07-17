0fbc32ad1807ed903bfaedb46211791019f7970a
Anamay Kumar Ojha <anamay.ojha@Anamay-Ojha-MAC.local>
2019-07-17 14:38:22 +0530

    android project redbustest

---
+++ app/src/main/java/com/example/redbustestapp/MainActivity.kt
@@ -0,0 +1,50 @@
+package com.example.redbustestapp
+
+import android.content.Intent
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.widget.EditText
+import android.widget.TextView
+import kotlinx.android.synthetic.main.activity_main.*
+
+class MainActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+
+
+
+        this.but_num.setOnClickListener {
+
+          val foj : Int = show()
+
+            val tex = findViewById<TextView>(R.id.texf)
+            tex.setText(foj.toString())
+
+            var foh : String = foj.toString()
+
+        val intd = Intent(this,Main2Activity::class.java)
+
+            intd.putExtra("hello" , foh)
+
+        startActivity(intd)
+        }
+
+
+    }
+
+   fun show() : Int {
+       val dog = findViewById<EditText>(R.id.editText)
+       val kol = findViewById<EditText>(R.id.editText1)
+
+       val gog :Int = dog.text.toString().toInt()
+       val fol : Int = kol.text.toString().toInt()
+       val sum : Int = gog + fol
+
+      return sum
+
+   }
+
+
+}
